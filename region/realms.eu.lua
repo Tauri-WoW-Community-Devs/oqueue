6b0eb7e49e7aa8ea5c97ff2e8fcc8aecc31ec5e9
Łukasz K <maczugapl@gmail.com>
2020-05-27 12:28:32 +0200

    Applied code styling to whole project. Few notes: - this is the only formatter that worked with VS Code for me - this is not ideal (4 spaces, no semicolor which is debatable) - 2 spaces with semicolor would be prefered for me.
    But hey, at least it's finally readable.
    PS: function oq.create_bounty_board(parent) couldn't be formatted.

--- region/realms.eu.lua
+++ region/realms.eu.lua
@@ -1,47 +1,34 @@
-﻿--[[ 
-  @file       realms.eu.lua
-  @brief      realm list for region: eu
+﻿local addonName, OQ = ...
 
-  @author     rmcinnis
-  @date       november 26, 2012
-  @par        copyright (c) 2012 Solid ICE Technologies, Inc.  All rights reserved.
-              this file may be distributed so long as it remains unaltered
-              if this file is posted to a web site, credit must be given to me along with a link to my web page
-              no code in this file may be used in other works without expressed permission  
-]]--
-local addonName, OQ = ... ;
+if (string.sub(GetCVar('realmList'), 1, 2) == 'hu') then
+    return
+end
 
-if (string.sub(GetCVar("realmList"),1,2) == "hu") then
- 
-OQ.REGION   = "hu";
-OQ.SK_BTAG  = "Maczuga";
-OQ.SK_NAME  = "Maczuga";
-OQ.SK_REALM = "[EN] Evermoon";
-OQ.DEFAULT_PREMADE_TEXT = "" ;
+OQ.REGION = 'hu'
+OQ.SK_BTAG = 'Maczuga'
+OQ.SK_NAME = 'Maczuga'
+OQ.SK_REALM = '[EN] Evermoon'
+OQ.DEFAULT_PREMADE_TEXT = ''
 
-OQ.BGROUP_ICON = {["Tauri Cross Realm"] = "Interface\\Icons\\Spell_Shadow_Misery"};
+OQ.BGROUP_ICON = {['Tauri Cross Realm'] = 'Interface\\Icons\\Spell_Shadow_Misery'}
 
-OQ.REALMNAMES_SPECIAL = {
-} ;
+OQ.REALMNAMES_SPECIAL = {}
 
 OQ.BGROUPS = {
-  ["Tauri Cross Realm"] = {
-    "[EN] Evermoon",
-    "[HU] Tauri WoW Server",
-    "[HU] Warriors of Darkness",
-  },
-} ;
+    ['Tauri Cross Realm'] = {
+        '[EN] Evermoon',
+        '[HU] Tauri WoW Server',
+        '[HU] Warriors of Darkness'
+    }
+}
 
 OQ.SHORT_BGROUPS = {
-	["[EN] Evermoon"] = 1, -- [1]
-	["[HU] Tauri WoW Server"] = 2, -- [2]
-  ["[HU] Warriors of Darkness"] = 3, -- [3]
+    ['[EN] Evermoon'] = 1, -- [1]
+    ['[HU] Tauri WoW Server'] = 2, -- [2]
+    ['[HU] Warriors of Darkness'] = 3, -- [3]
+    [1] = '[EN] Evermoon',
+    [2] = '[HU] Tauri WoW Server',
+    [3] = '[HU] Warriors of Darkness'
+}
 
-  [1] = "[EN] Evermoon",
-  [2] = "[HU] Tauri WoW Server",
-  [3] = "[HU] Warriors of Darkness",
-} ;
-
-
-OQ.gbl = {};
-end
+OQ.gbl = {}
